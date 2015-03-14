1464b9697babd8e81c46011a6bbe52fa4f7296ba
Santiago Carreno <santiagocarrenomendoza@gmail.com>
2015-03-13 19:09:30 -0500

    third commit

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,30 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file.
+//
+// Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require jquery
+//= require bootstrap
+//= require jquery_ujs
+//= require bootstrap-modal
+//= require bootstrap-modalmanager
+//= require turbolinks
+//= require application
+//= require init
+//= require campaigns
+
+//= bootstrap-sprockets
+
+
+
+
+//= require_tree .
+
+
